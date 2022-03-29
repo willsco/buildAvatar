b6d2e132b18c7f3db77d5794b0d6f272d3320f7c
willsco <williamgambas@gmail.com>
2022-03-29 17:54:28 -0300

    feat: add method to cut image

--- script.js
+++ script.js
@@ -19,6 +19,15 @@
     var element = doc.querySelector('.' + id);
     element.innerHTML = value;
   }
+  function setViewport(img, x, y, width, height) {
+    img.style.left = "-" + x + "px";
+    img.style.top  = "-" + y + "px";
+
+    if (width !== undefined) {
+        img.parentNode.style.width  = width  + "px";
+        img.parentNode.style.height = height + "px";
+    }
+}
 
   doc.addEventListener("DOMContentLoaded", function () {
     var phoneMask = IMask(
