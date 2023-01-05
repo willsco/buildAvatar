0541a31be8bb5b43d2466ab3e35816d9a6426cee
willsco <williamgambas@gmail.com>
2023-01-05 18:03:20 -0300

    changes

--- script.js
+++ script.js
@@ -7,8 +7,12 @@
   var withoutphoto = doc.querySelector('.without-photo');
   var name = doc.querySelector('.preparedName');
   var des_branch = doc.querySelector('#des-branch');
-
   var inputs = doc.querySelectorAll('form .form-control');
+
+  var url = 'assets/docs/manual.pdf';
+  var pdfjsLib = window['pdfjs-dist/build/pdf'];
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.2.146/pdf.worker.min.js';
+  var loadingTask = pdfjsLib.getDocument(url);
 
   for (var i = inputs.length - 1; i >= 0; i--) {
     inputs[i].addEventListener('keyup', updateSignature);
@@ -47,5 +51,29 @@
     html2canvas(signature, { letterRendering: 1, allowTaint: true, useCORS: true }).then(function (canvas) {
       assdown.href = canvas.toDataURL("image/png");
     });
+  });
+
+  loadingTask.promise.then(function (pdf) {
+    console.log('PDF loaded');
+    var pageNumber = 1;
+    pdf.getPage(pageNumber).then(function (page) {
+      console.log('Page loaded');
+      var scale = 1.5;
+      var viewport = page.getViewport({ scale: scale });
+      var canvas = document.getElementById('pdf-canvas');
+      var context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      var renderTask = page.render(renderContext);
+      renderTask.promise.then(function () {
+        console.log('Page rendered');
+      });
+    });
+  }, function (reason) {
+    console.error(reason);
   });
 })(window);
